cbf41a27349001a581b57c29b3edb1155b2b8b5a
sergiors99 <sergiorscr1999@gmail.com>
2023-06-27 14:27:33 +0200

    Update R&F_Project.py

--- R&F_Project.py
+++ R&F_Project.py
@@ -4,34 +4,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 from math import pi
 from streamlit_extras.dataframe_explorer import dataframe_explorer
-import io
-from PIL import Image
-import base64
-
-file = open(".../data/UN-Logo.png", "rb")
-contents = file.read()
-img_str = base64.b64encode(contents).decode("utf-8")
-buffer = io.BytesIO()
-file.close()
-img_data = base64.b64decode(img_str)
-img = Image.open(io.BytesIO(img_data))
-resized_img = img.resize((150, 60))  # x, y
-resized_img.save(buffer, format="PNG")
-img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-
-st.markdown(
-        f"""
-        <style>
-            [data-testid="stSidebarNav"] {{
-                background-image: url('data:image/png;base64,{img_b64}');
-                background-repeat: no-repeat;
-                padding-top: 50px;
-                background-position: 100px 50px;
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 
 st.sidebar.markdown("# UN POP Helpline")
 
