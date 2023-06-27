0b574321c276140da8ec43552d1955aa0805b36d
sergiors99 <sergiorscr1999@gmail.com>
2023-06-27 13:55:00 +0200

    update

--- R&F_Project.py
+++ R&F_Project.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from math import pi
+from streamlit_extras.app_logo import add_logo
 
-# Define the questions and their weights
+add_logo("data/UN_Logo.png", height=100)
+
 questions = [
     {
         'question': 'In which phase of the  do you feel your digital channel implementation is?',
@@ -204,15 +210,12 @@ questions = [
     }
 ]
 
-# Define the background image URL
-background_image_url = 'https://example.com/background_image.jpg'
+background_image_url = 'data/UN_Logo.png'
 
-# Streamlit app layout
 def app():
     st.title('Helpline Evaluation Test')
     st.markdown('The following test aims to evaluate how well is your helpline performing, after answering these 10 questions, you will get a final score from 0 to 5, and a series of recomendations based on your answers')
 
-    # Display questions and collect answers
     answers = {}
     for i, question in enumerate(questions):
         st.subheader(question['question'])
@@ -225,7 +228,6 @@ def app():
     # Calculate final score
     final_score = sum(answer['answer'] * question['weight'] for question, answer in zip(questions, answers.values()))
 
-    # Display final score and recommendations on submit
     if st.button('Submit'):
         st.subheader('Final Score')
         st.write(final_score)
@@ -234,6 +236,61 @@ def app():
         for i, answer in answers.items():
             st.markdown(f"{answer['recomendation']}", unsafe_allow_html=True)
 
-# Run the app
+uploaded_file = st.file_uploader("Please select a .csv file")
+
+data = pd.read_csv(uploaded_file)
+
+df = pd.DataFrame(data)
+
+def transform_priority(col):
+    if col.name == 'ExpertID':
+        return col
+    else:
+        return 10 - (col * 2)
+
+transformed_df = df.copy()
+priority_columns = df.columns[1:]
+transformed_df[priority_columns] = transformed_df[priority_columns].apply(transform_priority)
+
+transformed_df = pd.read_csv("transformed_data.csv")
+
+categories = list(transformed_df.columns[1:])
+N = len(categories)
+
+angles = [n / float(N) * 2 * pi for n in range(N)]
+angles += angles[:1]
+
+def update_chart(experts):
+    fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={'polar': True})
+    ax.set_theta_offset(pi / 2)
+    ax.set_theta_direction(-1)
+
+    for expert in experts:
+        if expert == "Average":
+            values = np.mean(transformed_df.iloc[:, 1:], axis=0).tolist()
+        else:
+            values = transformed_df[transformed_df['ExpertID'] == expert].values[0][1:].tolist()
+
+        values += values[:1]
+
+        if expert == "Average":
+            ax.plot(angles, values, linewidth=1, linestyle='solid', label="Average")
+            ax.fill(angles, values, alpha=0.25)
+        else:
+            ax.plot(angles, values, linewidth=1, linestyle='solid', label=expert)
+            ax.fill(angles, values, alpha=0.25)
+
+    plt.xticks(angles[:-1], categories)
+    plt.yticks([2, 4, 6, 8, 10], ["2", "4", "6", "8", "10"], color="grey", size=10)
+    plt.ylim(0, 10)
+    plt.title("Radar Chart for Experts", size=12)
+    st.pyplot(fig)
+
+expert_list = transformed_df['ExpertID'].tolist()
+expert_list.insert(0, "Average")
+selected_experts = st.multiselect("Select Experts:", expert_list, default=["Average"])
+
+update_chart(selected_experts)
+
 if __name__ == '__main__':
     app()
